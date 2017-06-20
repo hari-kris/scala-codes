636b27d9d5d897c0551334dd2f908b2b895e919d
hari-kris <hariganesh94@gmail.com>
2017-06-20 12:51:36 +0530

    Added another implementation for Armstrong number

--- Amstrong_number.scala
+++ Amstrong_number.scala
@@ -1,5 +1,14 @@
+// Implementation #1
 var number = 371
 number.toString.toList.map(_.asDigit).map(_.toDouble).map(pow(_,3)).sum.toInt.equals(number)
 
 Result
 res*: Boolean = true
+
+// Implementation #2
+number.toString.map(_.toChar.asDigit.toDouble).map(x=>pow(x,3)).sum.toInt.equals(number)
+
+Result
+res*: Boolean = true
+
+
